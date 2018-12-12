dbe3b0a68d059cb3ab5477f99aab35fb3d0b1e26
kenkoooo <kenkou.n@gmail.com>
2018-12-12 17:41:01 +0900

    Added script

---
+++ src/AtCoderBusiness.user.ts
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        AtCoder Business
+// @namespace   AtCoderBusiness
+// @description AtCoder in working time
+// @include     https://beta.atcoder.jp/contests/arc097/tasks/arc097_d
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+class Startup {
+    public static main(): number {
+        // document.getElementById("fixed-server-timer").remove();
+        document.querySelector<HTMLInputElement>("#main-container").style.boxShadow = "0px 0px 0px 0px #fff";
+        document.querySelector<HTMLInputElement>("#main-container").style.padding = "50px 0px 0px 0px";
+        document.querySelector<HTMLInputElement>("#main-div").style.background = "#fff";
+        document.querySelectorAll<HTMLInputElement>("p, span, h3, li").forEach(element => {
+            element.style.fontSize = "13px";
+            element.style.fontWeight = "normal";
+        });
+        document.querySelectorAll<HTMLInputElement>("pre").forEach(element => {
+            element.style.borderWidth = "0px 0px 0px 4px";
+            element.style.borderStyle = "solid";
+            element.style.borderLeftColor = "#ffeb8e";
+            element.style.backgroundColor = "#fff8dc";
+        });
+
+        let navbar_inverse = document.querySelector<HTMLInputElement>(".navbar-inverse");
+
+        navbar_inverse.style.backgroundColor = "#fff";
+        navbar_inverse.style.border = "0px";
+
+        let navbar_brand = document.querySelector<HTMLInputElement>(".navbar-brand");
+        navbar_brand.style.background = "url(\"https://cdn.sstatic.net/Img/unified/sprites.svg\")";
+        navbar_brand.style.width = "150px";
+        navbar_brand.style.height = "30px";
+        navbar_brand.style.backgroundPositionX = "0px";
+        navbar_brand.style.backgroundPositionY = "-500px";
+        return 0;
+    }
+}
+
+Startup.main();
